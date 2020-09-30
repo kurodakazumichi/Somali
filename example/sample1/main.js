c82c547c633a158b73b8d7d142a69f169dd6a3a6
kazumichi kuroda <kazumichi96@gmail.com>
2020-09-30 22:36:52 +0900

    Updated example

--- example/sample1/main.js
+++ example/sample1/main.js
@@ -1,4 +1,8 @@
 const config = new Somali.SceneConfig();
-config.id = "hoge";
-console.log(config);
+config.id = "graph1";
+config.width = 700;
 new Somali.Scene(config);
+
+const config2 = new Somali.SceneConfig();
+config2.id = "graph2";
+new Somali.Scene(config2);
